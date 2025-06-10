c25a9813cf1f7479d181d4237dbc7a122f039843
Gobind Singh <thesinghgobind@gmail.com>
2025-06-10 17:26:59 +0000

    init

--- storybook/src/stories/introduction/theme-shop/utils.ts
+++ storybook/src/stories/introduction/theme-shop/utils.ts
@@ -7,21 +7,7 @@ export const getThemeName = (str: string) =>
 export const copyToClipboard = async (text: string): Promise<void> => {
   return new Promise((resolve, reject) => {
     try {
-      const textArea = parent.document.createElement("textarea");
-      textArea.value = text;
-
-      textArea.style.position = "fixed";
-      textArea.style.top = "0";
-      textArea.style.left = "-9999px";
-      textArea.setAttribute("readonly", "true");
-
-      parent.document.body.appendChild(textArea);
-      textArea.select();
-
-      const successful = parent.document.execCommand("copy");
-      parent.document.body.removeChild(textArea);
-
-      return successful ? resolve() : reject(new Error("Copy failed!"));
+      parent.navigator.clipboard.writeText(text).then(resolve).catch(reject);
     } catch (err) {
       reject(new Error((err as string) ?? ""));
     }
