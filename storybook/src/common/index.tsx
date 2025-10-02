95ac5861c442916248df3ef7a47b29cbfcd5571a
theGobindSingh <thesinghgobind@gmail.com>
2025-10-03 00:55:12 +0530

    init

--- storybook/src/common/index.tsx
+++ storybook/src/common/index.tsx
@@ -3,20 +3,24 @@ import globalStyles from "@styles/global";
 import type { FC } from "react";
 import { Slide, ToastContainer } from "react-toastify";
 
-export const GlobalStyles: FC<unknown> = () => <Global styles={globalStyles} />;
+export const GlobalStyles: FC<unknown> = () => {
+  return <Global styles={globalStyles} />;
+};
 
-export const CustomToastContainer = () => (
-  <ToastContainer
-    position="bottom-center"
-    autoClose={2500}
-    hideProgressBar={false}
-    newestOnTop={false}
-    closeOnClick={true}
-    rtl={false}
-    pauseOnFocusLoss
-    draggable
-    pauseOnHover
-    theme="dark"
-    transition={Slide}
-  />
-);
+export const CustomToastContainer = () => {
+  return (
+    <ToastContainer
+      position="bottom-center"
+      autoClose={2500}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick={true}
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+      transition={Slide}
+    />
+  );
+};
